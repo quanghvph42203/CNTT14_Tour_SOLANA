e82e116cb7721e84b531d901a18c45ac905e57f5
quanghvph42203 <havietquangk4@gmail.com>
2024-11-03 21:27:59 +0700

    fix

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -3,17 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./global.css";
-import "./styles/style.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StyleProvider } from "@ant-design/cssinjs";
 import "react-toastify/dist/ReactToastify.css";
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-            <StyleProvider layer>
-                <App />
-            </StyleProvider>
-        </BrowserRouter>
-    </QueryClientProvider>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <StyleProvider layer>
+        <App />
+      </StyleProvider>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
